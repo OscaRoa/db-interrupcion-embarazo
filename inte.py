59d11e987f1c6e4ece171c5df0f7895f9a7b8b8a
Oscar Roa <oscaroa196@gmail.com>
2020-03-02 11:19:00 -0600

    Change url variable scope.

--- inte.py
+++ inte.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-url = "https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/download/?format=csv&timezone=America/Mexico_City&lang=es&use_labels_for_header=true&csv_separator=%2C"
-
 try:
     data = pd.read_csv('./data/interrupcion-legal-del-embarazo.csv')
 except FileNotFoundError:
+    url = "https://datos.cdmx.gob.mx/explore/dataset/interrupcion-legal-del-embarazo/download/?format=csv&timezone=America/Mexico_City&lang=es&use_labels_for_header=true&csv_separator=%2C"
     data = pd.read_csv(url)
     data.to_csv('./data/interrupcion-legal-del-embarazo.csv')
 
